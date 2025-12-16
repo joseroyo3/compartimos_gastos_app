823f3c61ff5c9dfe513dcdbfeb3391d1c66af2e3
jose <joseroyo3@gmail.com>
2025-12-16 11:16:49 +0100

    Batería de colores e implementarlo en Group Screen

--- lib/controllers/themes_controller.dart
+++ lib/controllers/themes_controller.dart
@@ -5,11 +5,27 @@ class ThemeController {
   // Especifico el verde del logo como color principal
   static const Color primaryColor = Color(0xFF30CBA1);
 
+  //LISTA DE COLORES DISPONIBLES PARA GRUPOS
+  static final List<Map<String, dynamic>> groupColors = [
+    {'color': Colors.blue, 'name': 'Azul'}, //1
+    {'color': Colors.red, 'name': 'Rojo'}, //2
+    {'color': Colors.green, 'name': 'Verde'}, //3
+    {'color': Colors.orange, 'name': 'Naranja'}, //4
+    {'color': Colors.purple, 'name': 'Morado'}, //5
+    {'color': Colors.teal, 'name': 'Verde azulado'}, //6
+    {'color': Colors.pink, 'name': 'Rosa'}, //7
+    {'color': Colors.indigo, 'name': 'Índigo'}, //8
+    {'color': Colors.amber, 'name': 'Ámbar'}, //9
+    {'color': Colors.cyan, 'name': 'Cian'}, //10
+    {'color': Colors.deepOrange, 'name': 'Naranja oscuro'}, //11
+    {'color': Colors.lightBlue, 'name': 'Azul claro'}, //12
+  ];
+
   // Creamos el Tema Global
   static final ThemeData tema = ThemeData(
     useMaterial3: true,
 
-    // definimos la paleta principal en verde, a futuro pasaremos otros colores (segun grupo)
+    // definimos la paleta principal en verde
     colorScheme: ColorScheme.fromSeed(
       seedColor: primaryColor,
       primary: primaryColor,
@@ -33,7 +49,6 @@ class ThemeController {
     ),
 
     // --- BOTÓN FLOTANTE ---
-    // Lo he puesto también en Verde (primaryColor) para que no dé error
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
       backgroundColor: primaryColor,
       foregroundColor: Colors.white,
